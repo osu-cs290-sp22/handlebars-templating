057457c7afe0c667ea84e4fa0c6a6d3cd8d800d7
Rob Hess <hessro@oregonstate.edu>
2022-05-23 11:31:16 -0700

    Add code from 5/23 lecture.

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 var express = require('express');
+var exphbs = require('express-handlebars')
+
+var peopleData = require('./peopleData.json')
+console.log("== peopleData:", peopleData)
 
 var port = process.env.PORT || 8000;
 var app = express();
+
+app.engine('handlebars', exphbs.engine({ defaultLayout: null }))
+app.set('view engine', 'handlebars')
 
 app.use(express.static('public'));
 
@@ -9,23 +16,53 @@ app.get('/people', function (req, res, next) {
   res.status(200).sendFile(__dirname + '/public/people.html');
 });
 
-var availablePeople = [
-  'luke',
-  'leia',
-  'rey',
-  'finn',
-  'r2d2'
-];
+// var availablePeople = [
+//   'luke',
+//   'leia',
+//   'rey',
+//   'finn',
+//   'r2d2'
+// ];
 
 app.get('/people/:person', function (req, res, next) {
   var person = req.params.person.toLowerCase();
-  if (availablePeople.indexOf(person) >= 0) {
-    res.status(200).sendFile(
-      __dirname + '/public/people/' + person + '.html'
-    );
+  var personData = peopleData[person]
+  if (personData) {
+    res.status(200).render('photoPage', personData)
   } else {
-    next();
+    next()
   }
+
+  // res.status(200).render('photoPage', {
+  //   renderTitleContainer: true,
+  //   name: "Princess Leia",
+  //   caption: "This is a caption that won't be used",
+  //   photos: [
+  //     {
+  //       url: "http://placekitten.com/480/480?image=1",
+  //       caption: "Leia as a cat #1"
+  //     },
+  //     {
+  //       url: "http://placekitten.com/480/480?image=2",
+  //       caption: "Leia as a cat #2"
+  //     },
+  //     {
+  //       url: "http://placekitten.com/480/480?image=3",
+  //       caption: "Leia as a cat #3"
+  //     },
+  //     {
+  //       url: "http://placekitten.com/480/480?image=4",
+  //       caption: "Leia as a cat #4"
+  //     }
+  //   ]
+  // })
+  // if (availablePeople.indexOf(person) >= 0) {
+  //   res.status(200).sendFile(
+  //     __dirname + '/public/people/' + person + '.html'
+  //   );
+  // } else {
+  //   next();
+  // }
 });
 
 app.get("*", function (req, res, next) {
